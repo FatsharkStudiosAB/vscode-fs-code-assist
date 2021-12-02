6ecfab19c561ac3947de1927b0b853e8bd34fef2
manuel.blanc <manuel.blanc@fatshark.se>
2021-12-02 16:49:03 +0100

    Colorize output (conflicts with other output colorization extensions); do not dispose of output (potential leak?)

--- src/connection-handler.ts
+++ src/connection-handler.ts
@@ -10,79 +10,80 @@ import { getTimestamp } from './utils';
 const MAX_CONNECTIONS = 32;
 
 export class ConnectionHandler {
-    _compiler?: StingrayConnection;
-    _game: Map<number, StingrayConnection>;
-    _connectionOutputs: Map<StingrayConnection, vscode.OutputChannel>;
+	_compiler?: StingrayConnection;
+	_game: Map<number, StingrayConnection>;
+	_connectionOutputs: Map<StingrayConnection, vscode.OutputChannel>;
 
-    constructor(){
-        this._game = new Map();     
-        this._connectionOutputs = new Map();
-    }
+	constructor(){
+		this._game = new Map();
+		this._connectionOutputs = new Map();
+	}
 
-    closeAll() {
-        this._compiler?.close();
-        for (let [port, game] of this._game) {
-            game.close();
-        }
-    }
+	closeAll() {
+		this._compiler?.close();
+		for (let [port, game] of this._game) {
+			game.close();
+		}
+	}
 
-    getCompiler() {
-        if (!this._compiler || this._compiler.isClosed()) {
-            this._compiler = new StingrayConnection(14032);
-            this._addOutputChannel("Stingray Compiler", this._compiler);
-        }
-        return this._compiler;        
-    }
+	getCompiler() {
+		if (!this._compiler || this._compiler.isClosed()) {
+			this._compiler = new StingrayConnection(14032);
+			this._addOutputChannel("Stingray Compiler", this._compiler);
+		}
+		return this._compiler;
+	}
 
-    getGame(port:number) {
-        let game = this._game.get(port);
-        if (!game || game.isClosed()) {
-            game = new StingrayConnection(port);
-            this._game.set(port, game);
-            this._addOutputChannel(`Stingray (${port})`, game);
-        }
-        return game;
-    }
+	getGame(port:number) {
+		let game = this._game.get(port);
+		if (!game || game.isClosed()) {
+			game = new StingrayConnection(port);
+			this._game.set(port, game);
+			this._addOutputChannel(`Stingray (${port})`, game);
+		}
+		return game;
+	}
 
-    connectAllGames(portStart:number, range:number) {
-        range = Math.min(range, MAX_CONNECTIONS);
-        for (let i = 0; i < range; ++i) {
-            this.getGame(portStart+i);
-        }
-    }
+	connectAllGames(portStart:number, range:number) {
+		range = Math.min(range, MAX_CONNECTIONS);
+		for (let i = 0; i < range; ++i) {
+			this.getGame(portStart+i);
+		}
+	}
 
-    getAllGames() {
-        let allGameConnections = [];
-        for (let [port, game] of this._game) {
-            if (!game.isClosed()) {
-                allGameConnections.push(game);
-            }
-        }
-        return allGameConnections;
-    }
+	getAllGames() {
+		let allGameConnections = [];
+		for (let [port, game] of this._game) {
+			if (!game.isClosed()) {
+				allGameConnections.push(game);
+			}
+		}
+		return allGameConnections;
+	}
 
-    getOutputForConnection(connection:StingrayConnection) {
-        return this._connectionOutputs.get(connection);
-    }
+	getOutputForConnection(connection:StingrayConnection) {
+		return this._connectionOutputs.get(connection);
+	}
 
-    _addOutputChannel(name:string, connection:StingrayConnection) {
-        const outputChannel = vscode.window.createOutputChannel(name);
-        connection.onDidReceiveData.add((response:any) => {
-            if (response.message) {
-                outputChannel.appendLine(`${getTimestamp()}  [${response.level}][${response.system}] ${response.message}`);
-            }
-        });
-        connection.onDidConnect.add(() => {
-            outputChannel.show();
-            vscode.commands.executeCommand("fatshark-code-assist._refreshConnectedClients");
-        });
-        connection.onDidDisconnect.add((hadError:boolean) => {
-            outputChannel.hide();
-            outputChannel.dispose();
-            vscode.commands.executeCommand("fatshark-code-assist._refreshConnectedClients");
-        });
-        this._connectionOutputs.set(connection, outputChannel);
-    }
+	_addOutputChannel(name:string, connection:StingrayConnection) {
+		let outputChannel: vscode.OutputChannel;
+		connection.onDidConnect.add(() => {
+			outputChannel = vscode.window.createOutputChannel(name);
+			outputChannel.show();
+			this._connectionOutputs.set(connection, outputChannel);
+			vscode.commands.executeCommand("fatshark-code-assist._refreshConnectedClients");
+		});
+		connection.onDidDisconnect.add((hadError:boolean) => {
+			outputChannel.hide();
+			//outputChannel.dispose();
+			vscode.commands.executeCommand("fatshark-code-assist._refreshConnectedClients");
+		});
+		connection.onDidReceiveData.add((response:any) => {
+			if (response.message) {
+				outputChannel.appendLine(`${getTimestamp()}  [${response.level}][${response.system}] ${response.message}`);
+			}
+		});
+	}
 }
 
 export let connectionHandler = new ConnectionHandler;
